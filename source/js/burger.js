8b64c4f5ce7d9a40fb6d47103d96397634cf1e90
el ganso <elganso45@gmail.com>
2021-01-13 22:07:46 +0300

    first commit

---
+++ source/js/burger.js
@@ -0,0 +1,26 @@
+let burger = document.querySelector('.burger');
+let nav = document.querySelector(".nav__list-nav");
+let b = document.querySelector('body')
+
+let b1 = document.querySelector(".b1");
+let b2 = document.querySelector(".b2");
+let b3 = document.querySelector(".b3");
+
+burger.addEventListener("click", f1);
+
+function f1() {
+  nav.classList.toggle("nav-active");
+  b1.classList.toggle("burger-active");
+  b2.classList.toggle("burger-active");
+  b3.classList.toggle("burger-active");
+}
+
+// bu.addEventListener('click', f1)
+
+// function f1() {
+//   a.style.display = 'block'
+//   b.style.position = 'fixed'
+// }
+
+
+
