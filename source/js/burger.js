302cca7bafb6e6cb9b8f841981fc941c419c9c05
el ganso <elganso45@gmail.com>
2021-01-21 04:27:45 +0300

    commit

--- source/js/burger.js
+++ source/js/burger.js
@@ -1,5 +1,5 @@
 let burger = document.querySelector('.burger');
-let nav = document.querySelector(".nav__list-nav");
+let navListNav = document.querySelector(".nav__list-nav");
 let b = document.querySelector('body')
 
 let b1 = document.querySelector(".b1");
@@ -8,19 +8,14 @@ let b3 = document.querySelector(".b3");
 
 
 burger.addEventListener("click", () => {
-  nav.classList.toggle("nav-active");
+  navListNav.classList.toggle("nav-active");
   b1.classList.toggle("burger-active");
   b2.classList.toggle("burger-active");
   b3.classList.toggle("burger-active");
   b.classList.toggle('body-active')
 });
 
-// bu.addEventListener('click', f1)
 
-// function f1() {
-//   a.style.display = 'block'
-//   b.style.position = 'fixed'
-// }
 
 
 
